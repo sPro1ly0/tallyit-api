d761cd4fd47e8ea53288157648d89a296e7dde09
Sabrina Becker <sbecka8@gmail.com>
2020-04-17 18:28:15 -0400

    Add eslint

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
@@ -19,21 +20,21 @@ app.use(cors());
 // );
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+  res.send('Hello, world!');
 });
 
 app.get('/api/*', (req, res) => {
-    res.json({ok: true});
+  res.json({ok: true});
 });
 
 app.use(function errorHandler(error, req, res, next) {
-    let response;
-    if (NODE_ENV=== 'production') {
-        response = { error: {message: 'server error' } };
-    } else {
-        response = { message: error.message, error };
-    }
-    res.status(500).json(response);
+  let response;
+  if (NODE_ENV=== 'production') {
+    response = { error: {message: 'server error' } };
+  } else {
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
 });
 
 module.exports = app;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: 'https://tallyit.now.sh',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://macarina@localhost/tallyit',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://macarina@localhost/tallyit-test'
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN: 'https://tallyit.now.sh',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://macarina@localhost/tallyit',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://macarina@localhost/tallyit-test'
 };
