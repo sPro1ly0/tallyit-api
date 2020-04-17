29fc4da2605dfa48dd5a9c190c456d812f63d848
Sabrina Becker <sbecka8@gmail.com>
2020-04-17 18:18:19 -0400

    Setup for testing

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,11 @@
+process.env.TZ = 'UCT';
+require('dotenv').config();
+
 const { expect } = require('chai');
 const supertest = require('supertest');
+
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+  'postgresql://macarina@localhost/tallyit-test';
 
 global.expect = expect;
 global.supertest = supertest;
